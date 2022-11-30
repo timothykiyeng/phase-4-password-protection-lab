10534a9394c5080770ace59cec25526bfce08439
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-30 18:46:15 +0300

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
 end
